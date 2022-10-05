7e052b16d60354c3c26aa9213c5f95a70b47d59f
Wellington Mendes <wellmend0@gmail.com>
2022-10-04 22:55:05 -0300

    [main] add documentation about the classes

--- app/src/data_capture.py
+++ app/src/data_capture.py
@@ -1,18 +1,62 @@
 from .stats import Stats
 
 class DataCapture:
+    """
+    Data Capture class, used for capturing data and for calling a Stats object.
+
+    ...
+
+    Attributes
+    ----------
+    __MAX_VALUE : int
+        maximum value of a single capture.
+    __map : list<int>
+        array of captures, where the index is the capture and the value is a accumulator.
+
+    Methods
+    -------
+    add(value)
+        add a capture to the map for accumulation.
+        has a time complexity of O(1).
+    build_stats()
+        call the Stats class to create an object that computes default statistics.
+    clear()
+        clear the __map attribute.
+    """
 
     def __init__(self, MAX_VALUE=1000):
         self.__MAX_VALUE = MAX_VALUE
         self.__map = [0]*self.__MAX_VALUE
 
     def add(self, value):
+        """Add a capture to the map for accumulation.
+
+        It has a time complexity of O(1) or constant.
+
+        Parameters
+        ----------
+        value : int
+            A single capture that must be positive integer less than __MAX_VALUE attribute
+
+        Raises
+        ------
+        ValueError
+            If the parameter isn't an integer or if the value ins't a positive integer less 
+            than __MAX_VALUE attribute.
+        """
         if type(value) != int or value < 0 or value >= 1000:
             raise ValueError("Value must a positive integer less than {}".format(self.__MAX_VALUE))
         self.__map[value]+=1
 
     def build_stats(self):
+        """Call Stats class constructor and return a Stats object.
+
+        The constructor has a time complexity of O(M), where M is the __MAX_VALUE attribute.
+        Since M is a constant equal to 1000 the time complexity is O(1) or constant.
+        """
         return Stats(self.__MAX_VALUE, self.__map)
 
     def clean(self):
+        """Clean the __map attribute and return to initial state.
+        """
         self.__map = [0]*self.__MAX_VALUE
--- app/src/stats.py
+++ app/src/stats.py
@@ -1,4 +1,27 @@
 class Stats:
+    """
+    Stats class, used for computing and accessing default statistics.
+
+    ...
+
+    Attributes
+    ----------
+    __less : list<int>
+        array that stores the less statistics based on some previous computations.
+    __greater : list<int>
+        array that stores the greater statistics based on some previous computations.
+
+    Methods
+    -------
+    __calculate_stats(value)
+        calculates the stats to be accessed later via public methods.
+    less()
+        returns the less statistics.
+    greater()
+        returns the greater statistics.
+    between()
+        returns the between statistics.
+    """
 
     def __init__(self, MAX_VALUE, map):
         self.__less = [0]*MAX_VALUE
@@ -6,18 +29,60 @@ class Stats:
         self.__calculate_stats(map)
 
     def __calculate_stats(self, map):
+        """Calculates the stats and put then on some arrays.
+
+        This function works by accessing map array and iteratively 
+        storing the accumalated values into the less and greater arrays.
+
+        It has a time complexity of O(M), where M is the number of elements in map.
+        Since M is a constant equal to 1000 the time complexity is O(1) or constant.
+
+        Parameters
+        ----------
+        map : list<int>
+            array of captures, defined on the DataCapture class.
+        """
         for i in range(1, len(map)):
             self.__less[i] = map[i-1]+self.__less[i-1]
         for i in range(len(map)-2, -1, -1):
             self.__greater[i] = map[i+1]+self.__greater[i+1]
 
     def less(self, value):
+        """Find the number of captures that are less than the given value.
+
+        It has a time complexity of O(1) or constant.
+
+        Parameters
+        ----------
+        value : int
+            value to be used as threshold
+        """
         return self.__less[value]
 
     def greater(self, value):
+        """Find the number of captures that are greater than the given value.
+
+        It has a time complexity of O(1) or constant.
+
+        Parameters
+        ----------
+        value : int
+            value to be used as threshold
+        """
         return self.__greater[value]
 
     def between(self, value1, value2):
+        """Find the number of captures that are between two given values.
+
+        It has a time complexity of O(1) or constant.
+
+        Parameters
+        ----------
+        value1 : int
+            value to be used as lower threshold
+        value2 : int
+            value to be used as upper threshold
+        """
         if value1 > value2:
             value1, value2 = value2, value1
         return self.__less[value2+1]-self.__less[value1]
