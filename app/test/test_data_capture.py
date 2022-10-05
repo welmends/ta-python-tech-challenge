0c9dd60421b99d969730ab204033b80ce8d1546e
Wellington Mendes <wellmend0@gmail.com>
2022-10-04 23:42:42 -0300

    [main] add some unit test cases

--- app/test/test_data_capture.py
+++ app/test/test_data_capture.py
@@ -1,16 +1,46 @@
 import unittest
 from app.src.data_capture import DataCapture
+from app.src.stats import Stats
 
 class TestDataCapture(unittest.TestCase):
 
-    def test_add(self):
-        pass
+    @classmethod
+    def setUpClass(self):
+        self.capture = DataCapture()
 
-    def test_build_stats(self):
-        pass
+    def test_add_success(self):
+        r1, r2, r3 = self.capture.add(0), self.capture.add(10), self.capture.add(999)
+        self.assertEqual(r1, None)
+        self.assertEqual(r2, None)
+        self.assertEqual(r3, None)
+
+    def test_add_fail_on_positive_value(self):
+        with self.assertRaises(ValueError):
+            self.capture.add(-10)
+
+    def test_add_fail_on_value_greater_or_equal_to_1000(self):
+        with self.assertRaises(ValueError):
+            self.capture.add(1000)
+
+    def test_add_fail_on_value_not_int(self):
+        with self.assertRaises(ValueError):
+            self.capture.add('10')
+
+    def test_build_stats_success(self):
+        self.capture.add(1)
+        self.capture.add(2)
+        stats = self.capture.build_stats()
+        self.assertIsInstance(stats, Stats)
+
+    def test_build_stats_success_on_empty_data_capture(self):
+        stats = self.capture.build_stats()
+        self.assertIsInstance(stats, Stats)
 
     def test_clean(self):
-        pass
+        self.capture.add(1)
+        self.capture.add(2)
+        r = self.capture.clean()
+        self.assertEqual(r, None)
 
 if __name__ == '__main__':
     unittest.main()
--- app/test/test_stats.py
+++ app/test/test_stats.py
@@ -1,16 +1,104 @@
 import unittest
+from app.src.data_capture import DataCapture
 from app.src.stats import Stats
 
 class TestStats(unittest.TestCase):
 
-    def test_less(self):
-        pass
+    @classmethod
+    def setUpClass(self):
+        capture = DataCapture()
+        capture.add(3)
+        capture.add(9)
+        capture.add(3)
+        capture.add(4)
+        capture.add(6)
+        self.stats = capture.build_stats()
 
-    def test_greater(self):
-        pass
+    def test_constructor(self):
+        stats = Stats(1000, [0]*1000)
+        self.assertIsInstance(stats, Stats)
+        
+    def test_less_on_success(self):
+        r = self.stats.less(4)
+        self.assertEqual(r, 2)
 
-    def test_between(self):
-        pass
+    def test_less_on_not_added_value(self):
+        r = self.stats.less(10)
+        self.assertEqual(r, 5)
+
+    def test_less_on_smallest_added_value(self):
+        r = self.stats.less(3)
+        self.assertEqual(r, 0)
+
+    def test_less_fail_on_negative_value(self):
+        with self.assertRaises(ValueError):
+            self.stats.less(-1)
+    
+    def test_less_fail_on_value_greater_or_equal_to_1000(self):
+        with self.assertRaises(ValueError):
+            self.stats.less(1000)
+
+    def test_less_fail_on_value_not_int(self):
+        with self.assertRaises(ValueError):
+            self.stats.less('10')
+
+    def test_greater_on_success(self):
+        r = self.stats.greater(4)
+        self.assertEqual(r, 2)
+
+    def test_greater_on_not_added_value(self):
+        r = self.stats.greater(2)
+        self.assertEqual(r, 5)
+
+    def test_greater_on_biggest_added_value(self):
+        r = self.stats.greater(9)
+        self.assertEqual(r, 0)
+
+    def test_greater_fail_on_negative_value(self):
+        with self.assertRaises(ValueError):
+            self.stats.greater(-1)
+    
+    def test_greater_fail_on_value_greater_or_equal_to_1000(self):
+        with self.assertRaises(ValueError):
+            self.stats.greater(1000)
+
+    def test_greater_fail_on_value_not_int(self):
+        with self.assertRaises(ValueError):
+            self.stats.greater('10')
+
+    def test_between_on_success(self):
+        r = self.stats.between(3, 6)
+        self.assertEqual(r, 4)
+
+    def test_between_on_equal_values(self):
+        r = self.stats.between(4, 4)
+        self.assertEqual(r, 1)
+
+    def test_between_on_not_included_values(self):
+        r = self.stats.between(2, 100)
+        self.assertEqual(r, 5)
+
+    def test_between_on_swapped_lower_and_upper(self):
+        r = self.stats.between(6, 3)
+        self.assertEqual(r, 4)
+
+    def test_between_fail_on_negative_value(self):
+        with self.assertRaises(ValueError):
+            self.stats.between(-1, -1)
+            self.stats.between(-1, 1)
+            self.stats.between(1, -1)
+    
+    def test_between_fail_on_value_greater_or_equal_to_1000(self):
+        with self.assertRaises(ValueError):
+            self.stats.between(1000, 1000)
+            self.stats.between(1000, 10)
+            self.stats.between(10, 1000)
+
+    def test_between_fail_on_value_not_int(self):
+        with self.assertRaises(ValueError):
+            self.stats.between('10', '10')
+            self.stats.between(10, '10')
+            self.stats.between('10', 10)
 
 if __name__ == '__main__':
     unittest.main()
