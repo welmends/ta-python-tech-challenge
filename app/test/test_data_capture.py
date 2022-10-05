b353c5ed4f0d2c561e854bc79a34f9e110a80c03
Wellington Mendes <wellmend0@gmail.com>
2022-10-04 22:56:12 -0300

    [main] add tests structure

---
+++ app/test/test_data_capture.py
@@ -0,0 +1,16 @@
+import unittest
+from app.src.data_capture import DataCapture
+
+class TestDataCapture(unittest.TestCase):
+
+    def test_add(self):
+        pass
+
+    def test_build_stats(self):
+        pass
+
+    def test_clean(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ app/test/test_stats.py
@@ -0,0 +1,16 @@
+import unittest
+from app.src.stats import Stats
+
+class TestStats(unittest.TestCase):
+
+    def test_less(self):
+        pass
+
+    def test_greater(self):
+        pass
+
+    def test_between(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
